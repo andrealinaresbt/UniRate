9d5b41f5711048649e9cdd3b54f07e74912118f2
Cesar Atencio <167229343+cesargatencio@users.noreply.github.com>
2025-09-25 16:47:22 -0400

    Base de ReactNative y ExpoGO

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Pantalla de inicio</Text>
+        <Text>Hola, esto es un HomeScreen básico Eskere.</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#F5F5F7',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
