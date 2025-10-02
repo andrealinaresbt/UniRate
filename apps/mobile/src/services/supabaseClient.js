de7caaa5829e0bc86596d4b42c0597a71950d57e
GabrielDBrito <britogabrielc@gmail.com>
2025-10-02 17:06:17 -0400

    api key fix

--- apps/mobile/src/services/supabaseClient.js
+++ apps/mobile/src/services/supabaseClient.js
@@ -2,8 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// credenciales Supabase
 const supabaseUrl = 'https://hsiwcusolcaicuxhnkcb.supabase.co';
-const supabaseAnonKey = '...';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzaXdjdXNvbGNhaWN1eGhua2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNjM4ODIsImV4cCI6MjA3NDgzOTg4Mn0.47wIP77NwDL01W3jAVFNTn4y8l7p8zmFt84kig3FVvo';
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
